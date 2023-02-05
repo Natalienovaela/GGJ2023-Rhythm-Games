f1cf31742d463b88873a5899cb381ff58979ab57
zsaviourch <zsaviour@character.xyz>
2023-02-05 14:12:02 +0530

    Added beatmap note reference

--- Assets/Engineering/Gameplay/GameManager.cs
+++ Assets/Engineering/Gameplay/GameManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+
+[System.Serializable]
+public class EventData
+{
+    public float time;
+    public string noteType;
+    public string noteKey;
+}
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextAsset beatMap; // JSON file reference
+    private EventData[] events; // Array of EventData objects
+
+    private float startTime;
+
+    private void Start()
     {
-        
+        // Read JSON file
+        string jsonData = beatMap.text;
+        events = JsonConvert.DeserializeObject<EventData[]>(jsonData);
+
+        startTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Loop through events array and print messages based on time
+        foreach (EventData ev in events)
+        {
+            if (Time.time - startTime >= ev.time)
+            {
+                Debug.Log(ev.noteType);
+                Debug.Log(ev.noteKey);
+            }
+        }
     }
 }
